ded89f0fb13645748eb2d3c4862072783863599d
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-03 10:42:37 +0200

    Initial commit

---
+++ If/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace If
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Skriv ditt namn:");
+      string name = Console.ReadLine();
+
+      if (name == "micke" || name == "niklas")
+      {
+        Console.WriteLine("Välkommen, mästare!");
+      }
+      else if (name == "herbert")
+      {
+        Console.WriteLine("Välkommen, skithög!");
+      }
+      else
+      {
+        Console.WriteLine("Vem är du? Du får inte komma in!");
+      }
+
+      // if (name != "micke")
+      // {
+      //   Console.WriteLine("Get lost, ya bum!");
+      // }
+
+
+      Console.ReadLine();
+    }
+  }
+}
